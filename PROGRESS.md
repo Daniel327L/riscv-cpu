6221a7ab6313be372664d4eb117906f890c85994
DanielL327 <danielleung327@gmail.com>
2026-07-26 15:59:03 -0400

    Completed reg file testbench, all test passed, updated progress log

--- PROGRESS.md
+++ PROGRESS.md
@@ -14,3 +14,13 @@
 ## Doing 
 - Working on testbench for register, (`tb_reg_file.v`), learning how to generate a periodic clock signal to test it since it contains synchronous logic. 
 
+> Day 2 - 2026-07-26
+
+## Done
+- Finished building register file testbench (`tb_reg_file.v`), learned how to build a testbench when synrchronous block is involved, such as time delay, how to generate a clock signal. Also, how to build a check function to check different cases, display results, use of system task and functions. The cases are not random, coming up cases that covers lots of scenarios, like what happen when your read address is 0 but enable is 1, in RISC-V, address 0 register always store the value 0, easier for doing arithmetic with 0.
+
+![](Images/reg_file_test.png)
+*REG_FILE Passing*
+
+## Doing
+- Moving on to imm_gen (`imm_gen.v`)
--- tb/tb_reg_file.v
+++ tb/tb_reg_file.v
@@ -1,6 +1,6 @@
-`timescale 1ns/1ps
+`timescale 1ns/1ps  //set time units and precision
 
-module tb_reg_file;
+module tb_reg_file;  //don't need (); since no real inputs and outputs or param, just reg type variables
 
 reg en;
 reg [4:0] a1_addr, a2_addr, rdaddr;
@@ -28,40 +28,59 @@ reg_file dut(  //device under test
 task check ( //every initial, always, begin end block runs sequentially
     input we,
     input [4:0] rd_addr,
-    input [31:0] rd_data,
-    input [31:0]exp_result
+    input [31:0] rd_data1,
+    input [31:0] rd_data2,
+    input [31:0]exp_result1,
+    input [31:0]exp_result2
 );
     begin
         rdaddr = rd_addr; 
         en = we;
-        rddata = rd_data;
+        rddata = rd_data1;
         @(posedge clk); //this syrnchronize the test script with the reg_file, so stuff get loaded in reg_file
         #1; //1 time unit delay before reading from either rs1 or rs2
         a1_addr = rd_addr;
+        rddata = rd_data2;
+        rdaddr = rd_addr + 32'd1; //at rosl pf overflow, if rd_addr is 5'd31, then because it's 5 bits, the LSB 5 bits will become 0.
+        @(posedge clk); 
+        #1; //post clock delay 
+        a2_addr = rd_addr + 32'd1;
+        #1; //delay before heading to if-else evaluation
         test = test + 1;
-        if(a1_data !== exp_result) begin
-            $display ("FAIL enable=%d address=%d got=%h but expected=%h", we, rd_addr, rd_data, exp_result);
+        if((a1_data !== exp_result1) || (a2_data !== exp_result2)) begin
+            $display ("FAIL we=%d addr=%d r1_in=%h r2_in=%h : r1=%h r2=%h but exp1=%h exp2=%h", we, rd_addr, rd_data1, rd_data2, 
+            a1_data, a2_data, exp_result1, exp_result2);
+
             error = error+1;
         end
         else begin
-            $display ("PASS enable=%d address=%d got=%h --> expected=%h", we, rd_addr, rd_data, exp_result);
+            $display ("PASS we=%d addr=%d r1_in=%h r2_in=%h : r1=%h r2=%h ---> exp1=%h exp2=%h", we, rd_addr, rd_data1, rd_data2, 
+            a1_data, a2_data, exp_result1, exp_result2);
+
         end     
     end
 endtask
 
 //-----stimulus section--------------------------
 
-initial begin
-    $dumpfile("tb_reg_file.vcd");
-    $dumpvars(0, tb_reg_file);
+initial begin 
+    $dumpfile("tb_reg_file.vcd"); //give a name to dump file for waveform
+    $dumpvars(0, tb_reg_file); //0 means evlauate all things under the scope of tb_reg_file
+    
+    check (1, 5'd0, 32'd1111, 32'd2222, 32'd0, 32'd2222); //follows exact order of the check function
+    check (1, 5'd10, 32'd15, 32'd16, 32'd15, 32'd16);
+    check (0, 5'd10, 32'd5555, 32'd6666, 32'd15, 32'd16);
 
-    $display("testing");
-
-
-end
-
-
-
-
+    $display("-------------------------------------------------");
+    $display ("Score: %0d/%0d", test-error, test);  //the 0 in %0d control padding space, adjust this number for custom width that comes before the numbers
+    
+    if (error == 0)  
+        $display("PASS");
+    else
+        $display("FAIL");
+    
+    $display("-------------------------------------------------");    
+    $finish; //need this to terminate, or else clocks run indefinitely 
+    end
 
 endmodule
